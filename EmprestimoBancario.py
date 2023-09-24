923cbbd66a83b377fec17c5d4cf19fcf5aef285d
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-09-24 14:24:46 -0300

    Create EmprestimoBancario.py

---
+++ EmprestimoBancario.py
@@ -0,0 +1,17 @@
+print('='*6,' Simulador de Financiamento de Imobiliário ','='*6)
+casa = float(input('Valor do Imóvel: R$ '))
+salario = float(input('Informe sua renda familiar mensal: R$ '))
+anos = int(input('Tempo de Financiamento: '))
+
+Prestação = casa/(anos * 12)
+minimo = salario * 30 / 100
+
+print('Para pagar uma casa de R$ {:.2f} em {} anos'.format(casa, anos), end = '')
+print('A prestação será de R$ {:.2f}'.format(Prestação))
+
+if casa <= minimo:
+    print('Empréstimo pode ser CONCEDIDO')
+else:
+    print('Empréstimo NEGADO!')
+
+print('='*64)
