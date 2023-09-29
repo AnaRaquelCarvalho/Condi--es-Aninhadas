291766bcf691d2816283f15701cb9ff1d7ef32d4
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-09-28 23:40:45 -0300

    Update Conversão.py
    Convertendo para Binário , Octal e Hexadecimal.

---
+++ Conversão.py
@@ -0,0 +1,17 @@
+print('='*16,' Base de Conversão ','='*16)
+
+n = int(input('Digite um número inteiro: '))
+print('''Escolha uma das bases para conversão: 
+[1] Converter para BINÁRIO
+[2] Converter para OCTAL
+[3] Converter para HEXADECIMAL''')
+opção = int(input('Sua Opção: '))
+
+if opção == 1:
+    print('{} convertido para Binário é igual a {}'.format(n, bin(n)))
+elif opção == 2:
+    print('{} convertido para OCTAL é igual a {}'.format(n, oct(n)))
+elif opção == 3:
+    print('{} convertido para HEXADECIMAL é igual a {}'.format(n,hex(n)))
+else:
+    print('>>> Opção Inválida. Tente novamente. <<<')
