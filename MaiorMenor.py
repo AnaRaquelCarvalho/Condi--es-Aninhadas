aa4b31eecf987f8049102139feaa6ff9219026e5
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-10-02 23:10:07 -0300

    Create MaiorMenor.py
    Comparando Números:
    Maior, Menor e Igual

---
+++ MaiorMenor.py
@@ -0,0 +1,11 @@
+print('='*12,' Comparando Números ', '='*12)
+n1 = int(input('Informe o 1º Número: '))
+n2 = int(input('Informe o 2º Número: '))
+if n1 > n2:
+    print('O Primeiro valor é MAIOR')
+elif n2 > n1:
+    print('O Segundo valor é MAIOR')
+else:
+    print('Não existe valor maior, os dois são IGUAIS')
+
+print('='*46)
