f34dfa16cdcdfe21e4e170a98026df47b4171f6e
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-10-14 10:52:26 -0300

    Update IMC.py
    Medidor de IMC - ÍNDICE DE MASSA CORPORAL.

--- IMC.py
+++ IMC.py
@@ -5,14 +5,14 @@ altura = float(input('Informe a altura(m): '))
 imc = peso / (altura ** 2)
 print('Seu IMC é de: {:.2f}  '.format(imc))
 if imc < 18.5:
-    print('Você está ABAIXO DO PESO!')
+    print('Você está ABAIXO DO PESO normal!')
 elif imc >= 18.5 and imc <= 25:
-    print('Você está no seu peso ideal!')
+    print('Você está no seu PESO ÍDEAL!')
 elif imc >= 25 and imc <= 30:
     print('Você está com SOBREPESO!')
 elif imc >= 30 and imc <= 40:
-    print('Você está com OBESIDADE!')
+    print('Você está com OBESIDADE, cuidado!')
 else:
-    print('Você está com OBESIDADE MÓRBIDA!')
+    print('Você está com OBESIDADE MÓRBIDA, cuidado!')
 
 print('='*46)
