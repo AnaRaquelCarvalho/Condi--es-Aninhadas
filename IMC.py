5691f958804424500603e463c74c45dcefdd7f20
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2023-10-14 11:17:09 -0300

    Update IMC.py

--- IMC.py
+++ IMC.py
@@ -7,7 +7,7 @@ print('Seu IMC é de: {:.2f}  '.format(imc))
 if imc < 18.5:
     print('Você está ABAIXO DO PESO normal!')
 elif imc >= 18.5 and imc <= 25:
-    print('Você está no seu PESO ÍDEAL!')
+    print('PARABÉNS, Você está no seu PESO ÍDEAL!')
 elif imc >= 25 and imc <= 30:
     print('Você está com SOBREPESO!')
 elif imc >= 30 and imc <= 40:
